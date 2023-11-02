f0a79bc378fcc165a11078ff048a79abd7f7dcc0
anton-tchekov <anton@tchekov.net>
2023-11-02 14:26:55 +0000

    Remove asserts and conversion function

--- lib/src/adc.h
+++ lib/src/adc.h
@@ -25,15 +25,4 @@ void adc_init(uint32_t adc);
  */
 int32_t adc_read(uint32_t adc, uint32_t channel);
 
-/**
- * @brief Convert ADC value to voltage in millivolts
- *
- * @param adc_value ADC value (0 - 4095)
- * @return Voltage in millivolts (0 - 3300)
- */
-static inline int32_t adc_to_mv(int32_t adc_value)
-{
-	return (adc_value * 3300) / 4095;
-}
-
 #endif /* __ADC_H__ */
