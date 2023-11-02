242f3123e67a66e6f25c08c8ac1e10cc92b00fa6
anton-tchekov <anton@tchekov.net>
2023-11-02 14:51:40 +0000

    Improve main

--- projects/iss-01/src/main.c
+++ projects/iss-01/src/main.c
@@ -19,28 +19,30 @@
 
 #define NUM_SAMPLES 4096
 
+static uint32_t nsample(uint32_t count)
+{
+	uint32_t value, i;
+	value = 0;
+	for(i = 0; i < count; ++i)
+	{
+		value += adc_read(2, 6);
+	}
+
+	return value / count;
+}
+
 static void adc_readloop(void)
 {
 	char buf[128];
 	int32_t value, distance;
-	uint32_t i;
-
 	for(;;)
 	{
-		value = 0;
-		for(i = 0; i < NUM_SAMPLES; ++i)
-		{
-			value += adc_read(2, 6);
-		}
-
-		value /= NUM_SAMPLES;
+		value = nsample(NUM_SAMPLES);
 		distance = gp2_get_distance(value);
-
-		uart_tx_str("Hello World!\n");
-
 		sprintf(buf, "%6d | %6d mm", value, distance);
+		uart_tx_str(buf);
+		uart_tx('\n');
 		font_str(10, 10, buf, COLOR_WHITE, COLOR_BLACK, Terminus16_Bold);
-		delay_ms(100);
 	}
 }
 
