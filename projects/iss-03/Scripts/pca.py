07d6b13180f97fbe9d3e14de311c7f7ead3a8f7b
Haronzs <im@keyth.me>
2024-01-11 18:01:48 +0100

    done

--- projects/iss-03/Scripts/pca.py
+++ projects/iss-03/Scripts/pca.py
@@ -5,28 +5,35 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Load your data
-data = pd.read_csv('itsboard_bewegunggradlinieg_3.csv')
+data = pd.read_csv('itsboard_mehrmalsgradlinieg_4.csv')
+time = data.iloc[:, 1]
 data = data.iloc[:, 2:]
 
-# Convert data to numpy array
-x = data
-
-
+# Standardize the data
 scaler = StandardScaler()
-x_standardized = scaler.fit_transform(x)
+x_standardized = scaler.fit_transform(data)
 
-# Scikit
-# Dim festlegen: pca = PCA(n_components=3)
-# sonst besser so:
+# Perform PCA
 pca = PCA()
 pca.fit(x_standardized)
+pca_score = pca.transform(x_standardized)
 pca_ratio = pca.explained_variance_ratio_
 eigVec = pca.components_
-pca_score = pca.transform(x)
+pca_score = pca.transform(x_standardized)
+
+print(eigVec)
 
 # Plot
 plt.bar(range(1, len(pca_ratio) + 1), pca_ratio)
 plt.xlabel('Principal Component')
-plt.ylabel('Explained Variance Ratio')
-plt.title('Explained Variance Ratio for Each Principal Component')
+plt.ylabel('Erklärte Varianz')
+plt.title('Erklärte Varianz Verhältnis für jeden Principal Component')
+plt.show()
+
+
+# Score plot
+plt.scatter(time, pca_score[:, 1])
+plt.xlabel('Zeit in Ticks(90MHz)')
+plt.ylabel('Principal Component')
+plt.title('Score Plot')
 plt.show()
